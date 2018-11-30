43e76c1ee9ffb30f37d864e48e1b33541a873bf9
gaojialin1987 <594616387@qq.com>
2018-11-30 14:56:40 +0800

    xunan commit

---
+++ Github/myblog/authorsay/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AuthorsayConfig(AppConfig):
+    name = 'authorsay'
---
+++ Github/myblog/myblog/urls.py
@@ -0,0 +1,29 @@
+"""myblog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # 注册子路由
+    path('album/', include('album.urls')),
+    path('article/', include('article.urls')),
+    path('author/', include('author.urls')),
+    path('authorsay/', include('authorsay.urls')),
+    path('comment/', include('comment.urls')),
+    path('message/', include('message.urls')),
+    path('photo/', include('photo.urls')),
+]
