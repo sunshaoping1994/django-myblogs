bc83f06c2c6bf5a7498bf73c3a0dcaf7b021a622
gaojialin1987 <594616387@qq.com>
2018-12-04 19:58:48 +0800

    修改登录路由

--- Github/myblog/myblog/views.py
+++ Github/myblog/myblog/views.py
@@ -5,6 +5,6 @@ from author.models import Author
 
 # 首页视图处理函数
 def index(request):
-    author_list = Author.objects.all().order_by('-create_time')
+    author_list = Author.objects.all().order_by('-create_time')[:4]
     return render(request, 'index.html', {'author_list': author_list})
 
