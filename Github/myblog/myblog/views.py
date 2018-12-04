c431b7f9d5330ac282b0094ae710aab65174fb72
gaojialin1987 <594616387@qq.com>
2018-12-04 19:54:20 +0800

    修改根路由

--- Github/myblog/myblog/views.py
+++ Github/myblog/myblog/views.py
@@ -1,8 +1,10 @@
 # 首页视图处理模块
 from django.shortcuts import render
+from author.models import Author
 
 
 # 首页视图处理函数
 def index(request):
-    return render(request, 'index.html')
+    author_list = Author.objects.all().order_by('-create_time')
+    return render(request, 'index.html', {'author_list':author_list})
 
